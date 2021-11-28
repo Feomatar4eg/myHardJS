0c0ff85229ecec269c94253c0f7b6f0eae60a867
Andrey Shmykov <feomatar4eg@gmail.com>
2021-11-28 18:41:47 +0300

    решение сложной задачи

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//begining
+let num = 266219;
+let str = num.toString();
+
+let p = 1;
+
+for (let i = 0; i < str.length; i++) {
+    p *= str[i];
+    
+}
+
+console.log(p);
+
+p = p ** 3;
+
+console.log(p);
+p = p.toString();
+console.log(p[0]+p[1]);
