19bf6215fb34ce25af6242eb0dfe4c23b7eef281
Andrey Shmykov <feomatar4eg@gmail.com>
2021-12-01 00:00:38 +0300

    сделал хард задачку 04

--- script.js
+++ script.js
@@ -1,70 +1,9 @@
 "use strict";
 
-let lang = 'ru';
-//let lang = 'en';
-//Первый способ
-if (lang == 'ru') {
-    console.log('Понедельник');
-    console.log('Вторник');
-    console.log('Среда');
-    console.log('Четверг');
-    console.log('Пятница');
-    console.log('Суббота');
-    console.log('Воскресенье');
-} else if (lang == 'en') {
-    console.log('Sunday');
-    console.log('Monday');
-    console.log('Tuesday');
-    console.log('Wednesday');
-    console.log('Thursday');
-    console.log('Friday');
-    console.log('Saturday');
-}
+const strFunc = function(str){
+    if (typeof str != 'string' ) {return 'Не строка';}
+    if (str.length >= 30) {return str.substring(0,31) + '...';}
+    return str.trim();
+};
 
-//Второй способ
-switch (lang) {
-    case 'ru': {
-    console.log('Понедельник');
-    console.log('Вторник');
-    console.log('Среда');
-    console.log('Четверг');
-    console.log('Пятница');
-    console.log('Суббота');
-    console.log('Воскресенье');
-    break;        
-    }
-    case 'en': {
-    console.log('Sunday');
-    console.log('Monday');
-    console.log('Tuesday');
-    console.log('Wednesday');
-    console.log('Thursday');
-    console.log('Friday');
-    console.log('Saturday');
-    break;
-    } 
-}
-
-//Третий способ
-
-lang = [['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'],['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']];
-
-//let i = 0; //для ру
-let i = 1 //для en
-
-for (let index = 0; index < lang[i].length; index++) {
-    console.log(lang[i][index]);
-    
-}
-
-
-
-//Задача вторая
-
-//let namePerson = 'Артем';
-let namePerson = 'Александр';
-//let namePerson = 'Андрей';
-
-(namePerson == 'Артем') ? console.log('Директор') : (namePerson == 'Александр') ? console.log('преподаватель') : console.log('студент');
-
-
+console.log(strFunc('00000000000000000000000000000000000000'));
